6f6047fd48c334ca51b1fa01346ffc997d74b972
ArcStatic <eband42@gmail.com>
2017-06-22 17:19:04 +0000

    update.sh now writes to events log correctly

--- serf-items/update.sh
+++ serf-items/update.sh
@@ -7,6 +7,8 @@ while read line
 do
   echo $line > ~/receivedtorrent.torrent
   echo $line >> /tmp/sent-torrents
+  date >> ~/events.log
+  echo "update" >> ~/events.log
 done < /dev/stdin
 
 echo "" >> /tmp/sent-torrents
