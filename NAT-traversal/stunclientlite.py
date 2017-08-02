6537a2c6d04d25c8dda2fb63c54a23695df7468b
ArcStatic <eband42@gmail.com>
2017-08-02 12:07:22 +0000

    Refined server/client scripts to use dictionaries instead of lists for storing participant details

--- NAT-traversal/stunclientlite.py
+++ NAT-traversal/stunclientlite.py
@@ -2,6 +2,7 @@ import os
 import sys
 import socket
 import time
+import ast
 
 ##### FUNCTION DEFINITIONS #####
 
@@ -10,13 +11,7 @@ def createsocket():
      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
      return s
 
-def createlistener(addr, port):
-    #UDP socket for IPv4
-     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-     s.bind((addr, port))
-     return s
-
-def getnatinfo(addr, port, s):
+def getnatinfo(addr, port, peercandidates, s):
     localaddress = s.getsockname()
     s.sendto("GetInfo: My locally detected address is %s on port %s" % (localaddress[0], localaddress[1]), (addr, port))
     print "Initial recv: %s" % s.recv(4096)
@@ -30,33 +25,47 @@ def getnatinfo(addr, port, s):
     external_port = external_port.split(": ")
     external_port = external_port[-1]
     time.sleep(1)
-    peerlist = s.recvfrom(4096)[0]
-    print "Peerlist: %s" % str(peerlist)
+    peerdata = s.recvfrom(4096)[0]
+    #print "Peerdata: %s" % str(peerdata)
+    peercandidates.update(ast.literal_eval(peerdata))
+    #print "Peercandidates: %s" % str(peercandidates)
     return external_ip, external_port
 
 
 #Keep the addr/port open to receive messages from other clients
-def keepaliveproxy(addr, port, s):
+def keepaliveproxy(addr, port, msg, peercandidates, s):
+    try:
+        peerdata = msg.split(" peerupdate ")
+        #print peerdata[1]
+        peercandidates.update(ast.literal_eval(peerdata[1]))
+        #print "Peercandidates: %s" % str(peercandidates)
+    except:
+        pass
     s.sendto("KeepAliveProxy ... ", (addr, port))
     time.sleep(5)
     
+    
 #Keep the addr/port open to receive messages from other clients
-def keepalivepeer(addr, port, msg, s):
+def keepalivepeer(addr, peers, msg, s):
     #msg format: "TalkTo 2.221.45.10 50120"
     msg = msg.split(" ")
     #msg[1] = address of peer
-    #int(msg[2]) = port number used by peer to server
-    s.sendto("KeepAlivePeer %s %d ... " % (addr, port), (msg[1], int(msg[2])))
-    print "Sent to %s %s" % (msg[1], msg[2])
+    #exp_port = port used by peer
+    ext_port = peers[msg[1]]
+    s.sendto("KeepAlivePeer %s ... " % addr, (msg[1], ext_port))
+    print "Sent to %s %d" % (msg[1], ext_port)
     time.sleep(5)    
 
 
 #Send response to server confirm communication with another client
-def sendresponse(addr, port, msg, s):
-    #msg format: "TalkTo 2.221.45.10 50120"
+def sendresponse(addr, port, peers, msg, s):
+    #msg format: "TalkTo 2.221.45.10 50120 ID 5"
+    #(ie. (MessageType) (IP-address) (port) ID (ID-number))
     msg = msg.split(" ")
     #msg[1] = address of peer
     #int(msg[2]) = port number used by peer to server
+#    peers[msg[1]] = int(msg[2])
+#    print peers
     s.sendto("RespondTo %s %s" % (msg[1], msg[2]), (addr, port))
     print "Response sent to %s %s" % (addr, msg[2])
 
@@ -67,7 +76,7 @@ def custommsg(string, addr, port, msg, s, peerlist):
     #msg[1] = address
     #int(msg[2]) = port number
     #Add [address, port] of peer to peerlist
-    peerlist.append([msg[1], msg[2]])
+    #peerlist.append([msg[1], msg[2]])
     s.sendto("CustomMsg %s %s %s" % (addr, port, string), (msg[1], int(msg[2])))
     print "Custom message sent to %s %s" % (msg[1], int(msg[2]))
 
@@ -84,12 +93,18 @@ def terminatesession(addr, port, msg, s):
 
 ###### ACTIVE SECTION OF SCRIPT #####
 
-#List of potential peers
-peerlist = []
+#List of received messages
+msglist = []
+
+#Dictionary of potential peers
+peercandidates = dict()
+
+#Dictionary of peers linked to in current session
+peerlist = dict()
 
 s = createsocket()
 #Arguments: address of proxy server, port, socket
-natinfo = getnatinfo(sys.argv[1], int(sys.argv[2]), s)
+natinfo = getnatinfo(sys.argv[1], int(sys.argv[2]), peercandidates, s)
 
 #True when KeepAlive with proxy is still required
 #False when independent session established between peers
@@ -99,32 +114,36 @@ proxycontact = True
 #False when no link confirmed between peers or session has been terminated
 sessionlink = False
 
+#ID to align correct responses with received messages
+msgid = 0
+
 #Main loop
 while True:
+#    print peercandidates
     msg = s.recvfrom(4096)[0]
     print "msg: %s" % msg
     #KeepAlive request/response cycle with server to keep UDP port open on local NAT
     #if "KeepAliveProxy" in msg:
     if ("KeepAliveProxy" in msg) and (proxycontact):
-        keepaliveproxy(sys.argv[1], int(sys.argv[2]), s)
+        keepaliveproxy(sys.argv[1], int(sys.argv[2]), msg, peercandidates, s)
         
-    #Catch incorrectly sent KeepAliveProxy messages
+    #Catch received KeepAliveProxy messages when proxycontact not enabled
     elif ("KeepAliveProxy" in msg) and (not proxycontact):
         print "Proxy contact disabled - KeepAliveProxy message not sent"
         
     #KeepAlive request/response cycle with peer to keep UDP port open on local NAT
     #elif "KeepAlivePeer" in msg:
     elif ("KeepAlivePeer" in msg) and (sessionlink):
-        keepalivepeer(natinfo[0], int(natinfo[1]), msg, s)
+        keepalivepeer(natinfo[0], peercandidates, msg, s)
         
-    #Catch incorrectly sent KeepAliveProxy messages
+    #Catch received KeepAliveProxy messages when sessionlink not enabled
     elif ("KeepAlivePeer" in msg) and (not sessionlink):
         print "Peer contact disabled - KeepAlivePeer message not sent"
         
     #TalkRequest sent from another peer through server
     #Sent in response to another peer wanting direct communication with this machine
     elif "TalkRequest" in msg:
-        sendresponse(sys.argv[1], int(sys.argv[2]), msg, s)
+        sendresponse(sys.argv[1], int(sys.argv[2]), peercandidates, msg, s)
         
     #TalkResponse sent from another peer through server
     #Sent to confirm another peer wanting direct communication with this machine
@@ -132,7 +151,7 @@ while True:
         proxycontact = False
         sessionlink = True
         #Send custom message to test traversal
-        custommsg(sys.argv[3], natinfo[0], int(natinfo[1]), msg, s, peerlist)
+        custommsg(sys.argv[3], natinfo[0], int(natinfo[1]), msg, s, peercandidates)
         
     #Independent session established with peer, set proxycontact to False
     #Stops KeepAlive cycle with proxy server
@@ -140,7 +159,7 @@ while True:
         proxycontact = False
         sessionlink = True
         #keepalivepeer(msg, s)
-        keepalivepeer(natinfo[0], int(natinfo[1]), msg, s) 
+        keepalivepeer(natinfo[0], peercandidates, msg, s) 
            
     #Terminate session established with peer, return to keepalive link with proxy server
     elif ("TerminateSession" in msg) and (sessionlink):
@@ -149,7 +168,7 @@ while True:
         proxycontact = True
         sessionlink = False
         terminatesession(natinfo[0], natinfo[1], msg, s)
-        keepaliveproxy(sys.argv[1], int(sys.argv[2]), s)
+        keepaliveproxy(sys.argv[1], int(sys.argv[2]), msg, peercandidates, s)
         
     #Catch excess TerminateSession messages
     elif ("TerminateSession" in msg) and (not sessionlink):
--- NAT-traversal/stunserverlite.py
+++ NAT-traversal/stunserverlite.py
@@ -10,48 +10,45 @@ def socketcreate(host, port):
     return s
 
 
-#Adds a peer to list of recent connections if not already present
+#Adds a peer to dictionary of recent clients
 #Doesn't return anything
-def addpeer(addr, port, list):
-    if [addr, port] not in list:
-        list.append([addr, port])
-    print list
+def addpeer(addr, port, peers):
+    #Add client address as key (str)
+    #Add client port as associated value (int)
+    peers[addr] = int(port)
+    print peers
 
 
-def keepalive(addr, port, s):
+def keepalive(addr, port, dictionary, s):
     print "\nMaintaining mapping to %s %d...\n" % (addr, port)
-    s.sendto("KeepAliveProxy ... ", (addr, port))
+    s.sendto("KeepAliveProxy peerupdate %s" % dictionary, (addr, port))
+    #Send up-to-date dictionary of potential peer candidates
+    #s.sendto("PeerCandidates update %s" % str(dictionary.items()), (addr, port))
 
 
-def talkto(addr, port, list, msg, s):
+def talkto(addr, port, peers, msg, s):
     #msg format: "TalkTo 2.221.45.10 50120"
     msg = msg.split(" ")
     #msg[1] = address
-    #Look for port associated with address
-    #list should probably be a dictionary instead
-    ext_port = " "
-    for i in range(len(list)):
-        #address found, retrieve port
-        if list[i][0] == msg[1]:
-            ext_port = list[i][1]
+    #Look for port associated with address in dictionary
+    ext_port = peers[msg[1]]
     try:
-        s.sendto("TalkRequest %s %d" % (addr, port), (msg[1], int(ext_port)))
+        s.sendto("TalkRequest %s %d" % (addr, port), (msg[1], ext_port))
+        print "sent TalkRequest %s %d to %s %d" % (addr, port, msg[1], ext_port)
     except:
         print "Error sending TalkRequest"
     
 
-def respondto(addr, port, list, msg, s):
+def respondto(addr, port, peers, msg, s):
     #msg format: "RespondTo 2.166.240.67 44512"
     msg = msg.split(" ")
     #msg[1] = address
     #Look for port associated with address
     ext_port = " "
-    for i in range(len(list)):
-        #address found, retrieve port
-        if list[i][0] == msg[1]:
-            ext_port = list[i][1]
+    ext_port = peers[msg[1]]
     try:
-            s.sendto("TalkResponse %s %d" % (addr, port), (msg[1], int(ext_port)))
+        s.sendto("TalkResponse %s %d" % (addr, port), (msg[1], ext_port))
+        print "sent TalkResponse %s %d to %s %d" % (addr, port, msg[1], ext_port)
     except:
         print "Error sending TalkResponse"
 
@@ -59,20 +56,20 @@ def respondto(addr, port, list, msg, s):
 #Main server loop
 #Takes a socket as argument 
 #Doesn't return anything
-def serverloop(s, list):
+def serverloop(s, dictionary):
     #Receive data and address
     msg, clientaddr = s.recvfrom(4096)
     #Keep connection alive between client and server
     if "KeepAliveProxy" in msg:
-        keepalive(clientaddr[0], clientaddr[1], s)
+        keepalive(clientaddr[0], clientaddr[1], dictionary, s)
     #Client requesting to talk to another client
     elif "TalkTo" in msg:
         print "Request received from %s %d: %s" % (clientaddr[0], clientaddr[1], msg)
-        talkto(clientaddr[0], clientaddr[1], list, msg, s)
+        talkto(clientaddr[0], clientaddr[1], dictionary, msg, s)
     #Client responding to a TalkTo request from another client
     elif "RespondTo" in msg:
         print "Request received from %s %d: %s" % (clientaddr[0], clientaddr[1], msg)
-        respondto(clientaddr[0], clientaddr[1], list, msg, s)
+        respondto(clientaddr[0], clientaddr[1], dictionary, msg, s)
     elif "GetInfo" in msg:
         print "\nReceived message from %s on port %s" % (clientaddr[0], clientaddr[1])
         print "\'%s\'" % msg
@@ -82,9 +79,9 @@ def serverloop(s, list):
         #Send received port number
         s.sendto("%s" % clientaddr[1], clientaddr)
         #Add peer to list of recent connections
-        addpeer(clientaddr[0], clientaddr[1], list)
-        #Send list of recent connections (with client's details omitted)
-        s.sendto(str(list[:-1]), clientaddr)
+        addpeer(clientaddr[0], clientaddr[1], dictionary)
+        #Send list of recent connections
+        s.sendto(str(dictionary.items()), clientaddr)
 #        return clientaddr[0], clientaddr[1]
         s.sendto("KeepAliveProxy ...", clientaddr)
     else:
@@ -99,8 +96,12 @@ def serverloop(s, list):
 #False data for local testing
 peerlist = [["2.126.122.29", 8990], ["81.4.56.190", 52708]]
 
+#Create dictionary for potential peers
+#Entry format - addr: port (str, int)
+peercandidates = dict()
+
 #Set up sockets
 s = socketcreate(sys.argv[1], int(sys.argv[2]))
 #Main server loop
 while True:
-    serverloop(s, peerlist)
+    serverloop(s, peercandidates)
