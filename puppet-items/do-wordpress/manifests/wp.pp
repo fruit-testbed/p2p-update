0ae5caeb96bf0ebb6bbf0826c6e14b61569bcbb7
Herry <herry13@gmail.com>
2017-09-04 00:26:54 +0100

    fixed bug: latest.tar.gz not found

--- puppet-items/do-wordpress/manifests/wp.pp
+++ puppet-items/do-wordpress/manifests/wp.pp
@@ -1,28 +1,40 @@
 class wordpress::wp {
 
-  #Copy wordpress bundle to /tmp
-  file{ '/tmp/latest.tar.gz':
-    ensure => present,
-    source => "puppet:///modules/wordpress/latest.tar.gz"
+  package { 'wget':
+    ensure => 'installed',
+  }
+
+  exec { 'download-wordpress':
+    cwd => '/tmp',
+    path => ['/bin', '/usr/bin'],
+    command => 'wget -q https://wordpress.org/latest.tar.gz',
+    require => Package['wget']
   }
 
   #Extract wordpress bundle
   exec { 'extract':
     cwd => "/tmp",
     command => "tar -xvzf latest.tar.gz",
-    require => File['/tmp/latest.tar.gz'],
+    require => Exec['download-wordpress'],
     path => ['/bin'],
   }
 
-  #Copy to /var/www
+  #Copy to /var/www/html
   exec { 'copy':
-    command => "cp -r /tmp/wordpress/* /var/www/",
+    command => "cp -r /tmp/wordpress/* /var/www/html",
     require => Exec['extract'],
     path => ['/bin'],
+    notify => Exec['cleanup']
+  }
+
+  exec { 'cleanup':
+    command => "rm -r /tmp/wordpress /tmp/latest.tar.gz /var/www/html/index.html || true",
+    path => ['/bin'],
+    subscribe => Exec['copy'],
   }
 
   #Generate wp-config.php file using a template
-  file { '/var/www/wp-config.php':
+  file { '/var/www/html/wp-config.php':
     ensure => present,
     require => Exec['copy'],
     content => template("wordpress/wp-config.php.erb")
