9c5dfc96e729cd5dacb1da360a9bb03c99bdd7ec
Herry <herry13@gmail.com>
2018-04-24 23:50:19 +0800

    implement ShellDeployer.deployZip()

--- update.go
+++ update.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -8,7 +9,9 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
@@ -317,6 +320,9 @@ func (sh ShellDeployer) deploy(filename string, d time.Duration) error {
 	if st.IsDir() {
 		return sh.deployDir(filename, d)
 	}
+	if strings.ToLower(filepath.Ext(filename)) == ".zip" {
+		return sh.deployZip(filename, d)
+	}
 	return sh.deployFile(filename, d)
 }
 
@@ -331,6 +337,67 @@ func (ShellDeployer) deployFile(filename string, d time.Duration) error {
 	err := cmd.Wait()
 	timer.Stop()
 	return err
+}
+
+func (sh ShellDeployer) deployZip(filename string, d time.Duration) error {
+	dir := path.Join(os.TempDir(), filename)
+	defer os.RemoveAll(dir)
+	_, err := Unzip(filename, dir)
+	if err != nil {
+		return fmt.Errorf("failed unzipping %s: %v", filename, err)
+	}
+	return sh.deployDir(dir, d)
+}
+
+// Unzip will decompress a zip archive, moving all files and folders
+// within the zip file (parameter 1) to an output directory (parameter 2).
+//
+// References:
+// - https://golangcode.com/unzip-files-in-go/
+// - https://stackoverflow.com/questions/20357223/easy-way-to-unzip-file-with-golang
+func Unzip(src string, dest string) ([]string, error) {
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+		defer rc.Close()
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+		} else {
+			// Make File
+			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+				return filenames, err
+			}
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return filenames, err
+			}
+			_, err = io.Copy(outFile, rc)
+
+			// Close the file without defer to close before next iteration of loop
+			outFile.Close()
+			if err != nil {
+				return filenames, err
+			}
+
+		}
+	}
+	return filenames, nil
 }
 
 func (sh ShellDeployer) deployDir(filename string, d time.Duration) error {
